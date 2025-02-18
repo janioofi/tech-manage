c2e26fc0fd041e485851b81224eb47b5f37d94ab
Janio Filho <janioofi@gmail.com>
2025-02-18 19:03:08 -0300

    First commit

---
+++ src/test/java/br/janioofi/techmanage/TechManageApplicationTests.java
@@ -0,0 +1,13 @@
+package br.janioofi.techmanage;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TechManageApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
