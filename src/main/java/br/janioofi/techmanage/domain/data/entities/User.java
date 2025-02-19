386aab738fc210e315f8e6bd4187a5eeab637dca
Janio Filho <janioofi@gmail.com>
2025-02-19 08:00:23 -0300

    fix: Added more validation annotations

--- src/main/java/br/janioofi/techmanage/domain/data/entities/User.java
+++ src/main/java/br/janioofi/techmanage/domain/data/entities/User.java
@@ -2,9 +2,12 @@ package br.janioofi.techmanage.domain.data.entities;
 
 import br.janioofi.techmanage.domain.data.enums.UserType;
 import br.janioofi.techmanage.domain.data.validation.PhoneNumber;
+import com.fasterxml.jackson.annotation.JsonFormat;
 import jakarta.persistence.*;
 import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Past;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -23,19 +26,25 @@ public class User {
 
     @NotNull
     @Column(nullable = false)
+    @NotBlank
     private String fullName;
 
     @Email
+    @NotNull
+    @NotBlank
     @Column(unique = true, nullable = false)
     private String email;
 
     @NotNull
     @PhoneNumber
+    @NotBlank
     @Column(nullable = false)
     private String phone;
 
+    @Past
     @NotNull
     @Column(nullable = false)
+    @JsonFormat(pattern = "yyyy-MM-dd")
     private Date birthDate;
 
     @NotNull
