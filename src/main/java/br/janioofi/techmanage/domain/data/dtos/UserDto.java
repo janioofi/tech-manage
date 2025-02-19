e9db82e17d2452d3aee04b94dd70ad5962972ff0
Janio Filho <janioofi@gmail.com>
2025-02-18 21:40:04 -0300

    fix: Fields are now required

--- src/main/java/br/janioofi/techmanage/domain/data/dtos/UserDto.java
+++ src/main/java/br/janioofi/techmanage/domain/data/dtos/UserDto.java
@@ -2,16 +2,16 @@ package br.janioofi.techmanage.domain.data.dtos;
 
 import br.janioofi.techmanage.domain.data.enums.UserType;
 import br.janioofi.techmanage.domain.data.validation.PhoneNumber;
-import com.fasterxml.jackson.annotation.JsonFormat;
 import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotNull;
 
 import java.util.Date;
 
 public record UserDto(
         Long id,
-        String fullName,
-        @Email String email,
-        @PhoneNumber String phone,
-        @JsonFormat(pattern = "yyyy-MM-dd") Date birthDate,
-        UserType userType) {
+        @NotNull String fullName,
+        @NotNull @Email String email,
+        @NotNull @PhoneNumber String phone,
+        @NotNull Date birthDate,
+        @NotNull UserType userType) {
 }
--- src/main/java/br/janioofi/techmanage/domain/data/entities/User.java
+++ src/main/java/br/janioofi/techmanage/domain/data/entities/User.java
@@ -2,32 +2,44 @@ package br.janioofi.techmanage.domain.data.entities;
 
 import br.janioofi.techmanage.domain.data.enums.UserType;
 import br.janioofi.techmanage.domain.data.validation.PhoneNumber;
-import com.fasterxml.jackson.annotation.JsonFormat;
 import jakarta.persistence.*;
 import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotNull;
+import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 import java.util.Date;
 
 @Entity
 @Data
 @Table(name = "TB_USER")
+@AllArgsConstructor
+@NoArgsConstructor
 public class User {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
+    @NotNull
+    @Column(nullable = false)
     private String fullName;
 
     @Email
-    @Column(unique = true)
+    @Column(unique = true, nullable = false)
     private String email;
 
+    @NotNull
     @PhoneNumber
+    @Column(nullable = false)
     private String phone;
 
-    @JsonFormat(pattern = "yyyy-MM-dd")
+    @NotNull
+    @Column(nullable = false)
     private Date birthDate;
 
+    @NotNull
     @Enumerated(EnumType.STRING)
+    @Column(nullable = false)
     private UserType userType;
 }
